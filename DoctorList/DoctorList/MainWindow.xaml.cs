82b54ac4f08fda6c9d8a085b52a24c36cb8525f9
Phu Quang <phuquang14722@gmail.com>
2021-10-22 21:05:21 +0700

    update

--- DoctorList/DoctorList/MainWindow.xaml.cs
+++ DoctorList/DoctorList/MainWindow.xaml.cs
@@ -16,53 +16,40 @@ using System.Windows.Controls;
 
 namespace DoctorList
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public List<Doctor> items = new List<Doctor>();
-        public bool isSelected = true;
+        public List<Doctor> doctors = new List<Doctor>();
         public int CheckedCount;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
-        }
-
-        public bool IsSelected
-        {
-            get { return isSelected; }
-            set { isSelected = value; OnPropertyChanged("IsSelected"); }
-        }
-
-        private string dataValue;
-
-        public string DataValue
-        {
-            get { return dataValue; }
-            set { dataValue = value; OnPropertyChanged("DataValue"); }
-        }
 
         public MainWindow()
         {
             InitializeComponent();
-            IsSelected = true;
-            DataValue = "Phu Quang";
             this.DataContext = this;
-            items.Add(new Doctor() { ID = 1, Name = "Quang 2k4", Age = 17, Phone = "02323432", Mail = "1@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2004, 1, 1)).ToString("dd/mm/yyyy") });
-            items.Add(new Doctor() { ID = 2, Name = "Quang 2k2", Age = 19, Phone = "02323432", Mail = "2@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
-            items.Add(new Doctor() { ID = 3, Name = "Lộc wibu", Age = 7, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
-            items.Add(new Doctor() { ID = 4, Name = "Nghĩa tay to", Age = 39, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
-            items.Add(new Doctor() { ID = 5, Name = "Tuấn khỉ", Age = 39, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
-            datagrid.ItemsSource = items;
+            doctors.Add(new Doctor() { ID = 1, Name = "Quang 2k4", Age = 17, Phone = "02323432", Mail = "1@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2004, 1, 1)).ToString("dd/mm/yyyy") });
+            doctors.Add(new Doctor() { ID = 2, Name = "Quang 2k2", Age = 19, Phone = "02323432", Mail = "2@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
+            doctors.Add(new Doctor() { ID = 3, Name = "Lộc wibu", Age = 7, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
+            doctors.Add(new Doctor() { ID = 4, Name = "Nghĩa tay to", Age = 39, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
+            doctors.Add(new Doctor() { ID = 5, Name = "Tuấn khỉ", Age = 39, Phone = "02323432", Mail = "3@kteam.com", Sex = SexType.Nam, Birthday = (new DateTime(2002, 1, 1)).ToString("dd/mm/yyyy") });
+            datagrid.ItemsSource = doctors;
             CheckedCount = 0;
         }
+
         public enum SexType { Nam, Nữ }
-        public class Doctor
+        public class Doctor : INotifyPropertyChanged
         {
-            public bool IsChecked { get; set; }
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string name)
+            {
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+
+            private bool isChecked;
+            public bool IsChecked
+            {
+                get { return isChecked; }
+                set { isChecked = value; OnPropertyChanged("IsChecked"); }
+            }
 
             public int ID { get; set; }
 
@@ -82,20 +69,23 @@ namespace DoctorList
         private void AllSelect_Checked(object sender, RoutedEventArgs e)
         {
             bool allcheckbox = (AllSelect.IsChecked == true);
-
+            for (int i = 0; i < doctors.Count; i++)
+                doctors[i].IsChecked = allcheckbox;
         }
 
         private void SingleCheckbox_Checked(object sender, RoutedEventArgs e)
         {
+            AllSelect.IsChecked = null;
             CheckBox c = (CheckBox)sender;
             if (c.IsChecked == true)
                 CheckedCount++;
             else
                 CheckedCount--;
-            if (CheckedCount == items.Count)
+            if (CheckedCount == doctors.Count)
                 AllSelect.IsChecked = true;
             else
-                AllSelect.IsChecked = false;
+                if (CheckedCount==0)
+                    AllSelect.IsChecked = false;
         }
     }
 }
